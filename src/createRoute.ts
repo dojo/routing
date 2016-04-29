42489a429dbd6dbf28a5c54bd497325fa963a1be
Mark Wubben <mark@novemberborn.net>
2016-04-29 18:12:28 +0100

    wip! code comments for createRoute

--- src/createRoute.ts
+++ src/createRoute.ts
@@ -9,31 +9,134 @@ import {
 	DeconstructedPath
 } from './util/path';
 
+/**
+ * Routes created without a `params()` function will receive a `params` object of this type.
+ */
 export interface DefaultParameters extends Parameters {
 	[param: string]: string;
 }
 
+/**
+ * Describes whether a route matched.
+ */
 export interface MatchResult<PP> {
+	/**
+	 * Whether there are path segments that weren't matched by this route.
+	 */
 	hasRemaining: boolean;
+
+	/**
+	 * Whether the route matched.
+	 */
 	isMatch: boolean;
+
+	/**
+	 * Position in the segments array that the remaining unmatched segments start.
+	 */
 	offset: number;
+
+	/**
+	 * Any extracted parameters. Only available if the route matched.
+	 */
 	params?: PP;
 }
 
+/**
+ * A route.
+ * The generic should be specified if parameter access is required.
+ */
 export interface Route<PP extends Parameters> {
+	/**
+	 * A deconstructed form of the path the route was created for. Used for matching.
+	 * @private
+	 */
 	path?: DeconstructedPath;
+
+	/**
+	 * Holds the next level of the route hierarchy.
+	 * @private
+	 */
 	routes?: Route<Parameters>[];
+
+	/**
+	 * Whether trailing slashes in the matching path must match trailing slashes in this route's path.
+	 * @private
+	 */
 	trailingSlashMustMatch?: boolean;
+
+	/**
+	 * Append one or more routes.
+	 * @param routes A single route or an array containing 0 or more routes
+	 */
 	append(routes: Route<Parameters> | Route<Parameters>[]): void;
+
+	/**
+	 * Callback used to execute the route if it's been selected.
+	 * @param request An object whose `context` property contains the dispatch context. Extracted parameters are
+	 *   available under `params`.
+	 * @protected
+	 */
 	exec(request: Request<PP>): void;
+
+	/**
+	 * Optional fallback handler used when no nested routes matched.
+	 * @param request An object whose `context` property contains the dispatch context. No extracted parameters
+	 *   are available.
+	 * @protected
+	 */
 	fallback?(request: Request<PP>): void;
+
+	/**
+	 * Callback used to determine whether the route should be selected after it's been matched.
+	 * @param request An object whose `context` property contains the dispatch context. Extracted parameters are
+	 *   available under `params`.
+	 * @private
+	 */
 	guard(request: Request<PP>): boolean;
+
+	/**
+	 * Optional callback used to execute the route if it's been selected as the final route in the hierarchy.
+	 * Takes precedence over exec() in this scenario.
+	 * @param request An object whose `context` property contains the dispatch context. Extracted parameters are
+	 *   available under `params`.
+	 * @protected
+	 */
 	index?(request: Request<PP>): void;
+
+	/**
+	 * Determine whether the route matches.
+	 * @param segments Segments of the pathname (excluding slashes)
+	 * @param hasTrailingSlash Whether the pathname that's being matched ended with a slashes
+	 * @param searchParams Parameters extracted from the search component
+	 * @return Whether and how the route matched
+	 * @private
+	 */
 	match(segments: string[], hasTrailingSlash: boolean, searchParams: UrlSearchParams): MatchResult<PP>;
-	params(fromPath: string[], searchParams: UrlSearchParams): void | PP;
+
+	/**
+	 * Callback used for constructing the `params` object from extracted parameters, and validating the parameters.
+	 * @param fromPathname Array of parameter values extracted from the pathname
+	 * @param searchParams Parameters extracted from the search component
+	 * @return If `null` prevents the route from being selected, else the value for the `params` object
+	 * @private
+	 */
+	params(fromPathname: string[], searchParams: UrlSearchParams): void | PP;
+
+	/**
+	 * Attempt to select this and any nested routes.
+	 * @param context The dispatch context
+	 * @param segments Segments of the pathname (excluding slashes)
+	 * @param hasTrailingSlash Whether the pathname that's being matched ended with a slashes
+	 * @param searchParams Parameters extracted from the search component
+	 * @return An empty array if this (and any nested routes) could not be selected, else the selected routes and
+	 *   accompanying `params` objects
+	 */
 	select(context: Context, segments: string[], hasTrailingSlash: boolean, searchParams: UrlSearchParams): Selection[];
 }
 
+/**
+ * The options for the route.
+ */
 export interface RouteOptions<PP> {
 	path?: string;
 	trailingSlashMustMatch?: boolean;
@@ -41,7 +144,7 @@ export interface RouteOptions<PP> {
 	fallback?(request: Request<PP>): void;
 	guard?(request: Request<PP>): boolean;
 	index?(request: Request<PP>): void;
-	params?(fromPath: string[], searchParams: UrlSearchParams): void | PP;
+	params?(fromPathname: string[], searchParams: UrlSearchParams): void | PP;
 }
 
 export const enum ExecutionMethod { Exec, Fallback, Index }
@@ -99,12 +202,12 @@ const createRoute: RouteFactory = compose({
 		return { hasRemaining, isMatch: true, offset, params };
 	},
 
-	params (fromPath: string[], searchParams: UrlSearchParams): DefaultParameters {
+	params (fromPathname: string[], searchParams: UrlSearchParams): DefaultParameters {
 		const params: DefaultParameters = {};
 
 		const { parameters, searchParameters } = <DeconstructedPath> this.path;
 		parameters.forEach((name, index) => {
-			params[name] = fromPath[index];
+			params[name] = fromPathname[index];
 		});
 		searchParameters.forEach(name => {
 			if (searchParams.has(name)) {
