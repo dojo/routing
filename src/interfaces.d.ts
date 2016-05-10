d4348706a583bffdd2ef50a0486ad423a6f41e23
Mark Wubben <mark@novemberborn.net>
2016-05-10 16:25:00 +0100

    code comments for interfaces

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,12 +1,28 @@
+/**
+ * Describes the context object used when dispatching. Extend this interface for custom contexts.
+ */
 export interface Context {
 	// TODO: Does specifiying an indexer make sense here?
 }
 
+/**
+ * Describes extracted parameters.
+ */
 export interface Parameters {
 	// TODO: Does specifying an indexer make sense here?
 }
 
+/**
+ * Describes the object passed to various route handlers.
+ */
 export interface Request<PP extends Parameters> {
+	/**
+	 * The dispatch context.
+	 */
 	context: Context;
+
+	/**
+	 * The extracted parameters.
+	 */
 	params: PP;
 }
